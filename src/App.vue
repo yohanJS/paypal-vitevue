72f9d8a7bba04d63853d34dd88d87f913cb8874c
Yohan Garcia <yoanvaldes03@gmail.com>
2024-04-24 16:26:20 -0400

    - removing the icons

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@ import PayPalButtons from './components/PayPalButtons.vue';
 
 <template>
   <div class="bg-white">
-    <img src="/vite.svg" class="logo" alt="Vite logo" />
-    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     <h3>PayPal SDK Integration</h3>
   </div>
   <NavBar></NavBar>
